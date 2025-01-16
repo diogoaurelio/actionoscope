fc61137efe3002b0bbbe850d7a308a5e666e54a6
Diogo Aurelio <diogo@mklabs.io>
2025-01-16 19:37:21 +0100

    feat(cli): relax on constraint of having to pass which workflow to run
    .. automatically infer which workflows should be run by listing .github/workflows directory
    
    Signed-off-by: Diogo Aurelio <diogo@mklabs.io>

--- actionoscope-cli/src/main.rs
+++ actionoscope-cli/src/main.rs
@@ -15,7 +15,7 @@ use actionoscope::Workflow;
 struct Cli {
     /// Path to the workflow YAML file
     #[arg(long, short = 'w')]
-    workflow_file: String,
+    workflow_file: Option<String>,
 
     /// Job name to run
     #[arg(long, short = 'j')]
@@ -43,17 +43,42 @@ fn validate_workflow_file(workflows_dir: &Path, workflow_file: &str) -> Option<P
     }
 }
 
-fn find_workflow_file(workflow_file: &str) -> Option<PathBuf> {
+fn find_workflow_files(
+    workflow_file: Option<String>,
+) -> Result<Vec<PathBuf>, Box<dyn std::error::Error>> {
     let current_dir = Path::new(".");
-    let workflow_path = validate_workflow_file(&current_dir, workflow_file);
+    let workflows_dir = current_dir.join(".github").join("workflows");
+    let mut result = Vec::new();
+    if let Some(workflow_file) = workflow_file {
+        // check if the file exists in the current directory
+        let workflow_path = validate_workflow_file(current_dir, &workflow_file);
+        if let Some(path) = workflow_path {
+            result.push(path);
+            return Ok(result);
+        }
 
-    if workflow_path.is_some() {
-        return workflow_path;
+        // fallback: check if the file exists in the .github/workflows directory
+        let exists = validate_workflow_file(&workflows_dir, &workflow_file);
+        if let Some(path) = exists {
+            result.push(path);
+            return Ok(result);
+        }
+        let err = format!("Provided workflow file {} was not found", workflow_file);
+        return Err(err.into());
+    }
+    for entry in fs::read_dir(workflows_dir)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_file() {
+            result.push(path);
+        }
     }
 
-    let workflows_dir = current_dir.join(".github").join("workflows");
-
-    validate_workflow_file(&workflows_dir, workflow_file)
+    if !result.is_empty() {
+        Ok(result)
+    } else {
+        Err("No workflow files found in .github/workflows directory".into())
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -72,42 +97,48 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         std::process::exit(1);
     }
 
-    let workflow_file = find_workflow_file(&cli.workflow_file).unwrap_or_else(|| {
-        error!("Workflow file '{}' not found", &cli.workflow_file);
-        std::process::exit(1);
-    });
+    let workflow_files = find_workflow_files(cli.workflow_file)?;
 
-    info!("Found workflow file: {}", workflow_file.to_string_lossy());
+    info!(
+        "Found workflow file(s): {}",
+        workflow_files
+            .iter()
+            .map(|w| w.to_string_lossy())
+            .collect::<Vec<_>>()
+            .join(", ")
+    );
 
-    let workflow_content = fs::read_to_string(workflow_file.to_string_lossy().into_owned())
-        .unwrap_or_else(|err| {
-            error!("Failed to read the workflow file: {}", err);
+    for workflow_file in &workflow_files {
+        let workflow_content = fs::read_to_string(workflow_file.to_string_lossy().into_owned())
+            .unwrap_or_else(|err| {
+                error!("Failed to read the workflow file: {}", err);
+                std::process::exit(1);
+            });
+
+        let workflow = Workflow::from_yaml(&workflow_content).unwrap_or_else(|err| {
+            error!("Failed to parse the workflow file: {}", err);
             std::process::exit(1);
         });
 
-    let workflow = Workflow::from_yaml(&workflow_content).unwrap_or_else(|err| {
-        error!("Failed to parse the workflow file: {}", err);
-        std::process::exit(1);
-    });
+        let job = workflow.get_job(&cli.job).unwrap_or_else(|| {
+            error!("Job '{}' not found in the workflow", cli.job);
+            std::process::exit(1);
+        });
 
-    let job = workflow.get_job(&cli.job).unwrap_or_else(|| {
-        error!("Job '{}' not found in the workflow", cli.job);
-        std::process::exit(1);
-    });
+        let step_name: &str = &cli.step.as_deref().or(cli.from_step.as_deref()).unwrap();
+        let first_step = job.get_step(step_name).unwrap_or_else(|| {
+            error!("Step '{}' not found in the job '{}'", step_name, cli.job);
+            std::process::exit(1);
+        });
 
-    let step_name: &str = &cli.step.as_deref().or(cli.from_step.as_deref()).unwrap();
-    let first_step = job.get_step(step_name).unwrap_or_else(|| {
-        error!("Step '{}' not found in the job '{}'", step_name, cli.job);
-        std::process::exit(1);
-    });
-
-    if cli.step.is_some() {
-        first_step.run_cmd(workflow.env.clone())?;
-    } else {
-        for step in &job.get_all_steps_since(step_name, cli.to_step.as_deref()) {
-            if let Err(e) = step.run_cmd(workflow.env.clone()) {
-                error!("Error running step '{}': {}", step.get_name_or_id(), e);
-                std::process::exit(1);
+        if cli.step.is_some() {
+            first_step.run_cmd(workflow.env.clone())?;
+        } else {
+            for step in &job.get_all_steps_since(step_name, cli.to_step.as_deref()) {
+                if let Err(e) = step.run_cmd(workflow.env.clone()) {
+                    error!("Error running step '{}': {}", step.get_name_or_id(), e);
+                    std::process::exit(1);
+                }
             }
         }
     }
